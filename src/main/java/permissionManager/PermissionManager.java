ff7e8ffe36c000f6c822675e41a8d80c15236c29
Sevar Jafarli <71344192+SevarJafarli@users.noreply.github.com>
2022-09-28 22:18:39 +0400

    Update PermissionManager.java

--- src/main/java/permissionManager/PermissionManager.java
+++ src/main/java/permissionManager/PermissionManager.java
@@ -1,32 +1,17 @@
 package permissionmanager;
-import permissionlevel.PermissionLevel;
+
+ enum PermissionLevel {
+    ADMIN, DEVELOPER, USER;
+}
+
 public class PermissionManager {
     private  PermissionLevel mCurrentLevel = PermissionLevel.USER;
-
-    public PermissionLevel get() {
-        return mCurrentLevel;
-      }
+  
     public void set(PermissionLevel level){
         this.mCurrentLevel = level;
-    }
+    } 
 
-    public String getPermissionLevel() {
-         String level = "USER";
-        switch(mCurrentLevel){
-            case ADMIN:
-            level = "ADMIN";
-            break;
-            case DEVELOPER:
-            level = "DEVELOPER";
-            break;
-            case USER:
-            level = "USER";
-            break;
-            default:
-            level = "USER";
-            break;
-        }
-        return level;
-        
+    public String get(){
+        return this.mCurrentLevel.name();
     }
 }
